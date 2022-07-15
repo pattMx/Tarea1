832e542dcf1a749680aa5465ceac6f5a30c6027d
PATRICIA <paty_kuro@hotmail.com>
2022-07-15 21:52:41 +0200

    functional

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        suma(10,15,20);
+    }
+    public static void suma (int a, int b, int c) {
+        int resultado = a + b + c;
+        System.out.println(resultado);
+
+    }
+
+
+
+}
